4f02f514684d9b09b10d84d89cf7a6cd9ecc42d1
Antônio Rocha <antoniojunior159@gmail.com>
2024-05-23 22:42:55 -0300

    Implementação do Swagger na API

--- api/src/core/application/dtos/customer/update-customer.dto.ts
+++ api/src/core/application/dtos/customer/update-customer.dto.ts
@@ -1,6 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { RequestCustomerDto } from './request-customer.dto';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class UpdateCustomerDto extends PartialType(RequestCustomerDto) {
+    @ApiProperty()
     id: number;
 }
