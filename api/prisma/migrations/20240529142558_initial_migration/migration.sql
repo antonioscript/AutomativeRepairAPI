7132d43ec95df0ccc88a8b8995e8d14cbef1a869
Antônio Rocha <antoniojunior159@gmail.com>
2024-05-29 11:27:56 -0300

    Config

--- api/prisma/migrations/20240529142558_initial_migration/migration.sql
+++ api/prisma/migrations/20240529142558_initial_migration/migration.sql
@@ -3,6 +3,8 @@ CREATE TABLE `customers` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `firstName` VARCHAR(100) NOT NULL,
     `lastName` VARCHAR(100) NOT NULL,
+    `cpf` VARCHAR(15) NOT NULL,
 
+    UNIQUE INDEX `customers_cpf_key`(`cpf`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
