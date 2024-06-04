6868c948089a5464792976ea019f9f9020e7f5e9
Antônio Rocha <antoniojunior159@gmail.com>
2024-06-04 20:58:28 -0300

    Organizando os controllers por ordem alfabética

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,31 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  //CORS
+  // CORS
   app.enableCors();
 
-  //Class Validator
+  // Class Validator
   app.useGlobalPipes(new ValidationPipe());
 
-  //Swagger
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('AutomativeRepairAPI')
     .setDescription('Documentação da API construída em Nest.JS que simula operações de uma assistência mecânica')
     .setVersion('0.1')
     .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document, {
-      swaggerOptions: {
-        docExpansion: 'none' 
-      },
-    });
+  const document = SwaggerModule.createDocument(app, config);
+
+  document.paths = sortPathsAlphabetically(document.paths);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      docExpansion: 'none' 
+    },
+  });
 
   await app.listen(3000);
 }
+
+function sortPathsAlphabetically(paths: OpenAPIObject['paths']): OpenAPIObject['paths'] {
+  const sortedPaths: OpenAPIObject['paths'] = {};
+  const sortedPathsKeys = Object.keys(paths).sort();
+  sortedPathsKeys.forEach(key => {
+    sortedPaths[key] = paths[key];
+  });
+  return sortedPaths;
+}
+
 bootstrap();
