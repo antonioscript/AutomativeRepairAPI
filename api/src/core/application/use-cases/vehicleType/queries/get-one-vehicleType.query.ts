cd989802b161af7841bef7505be86b2f28d8a8b6
Antônio Rocha <antoniojunior159@gmail.com>
2024-06-12 21:41:17 -0300

    Criando Container da Aplicacao

--- api/src/core/application/use-cases/vehicleType/queries/get-one-vehicleType.query.ts
+++ api/src/core/application/use-cases/vehicleType/queries/get-one-vehicleType.query.ts
@@ -1,7 +1,7 @@
 import { NotFoundException } from "@nestjs/common";
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
-import { ResponseVehicleTypeDto } from "src/core/application/dtos/VehicleType/response-VehicleType.dto";
-import { ResponseVehicleTypeMapper } from "src/core/domain/mapping/VehicleType/response-VehicleType.mapper";
+import { ResponseVehicleTypeDto } from "src/core/application/dtos/vehicleType/response-vehicleType.dto";
+import { ResponseVehicleTypeMapper } from "src/core/domain/mapping/vehicleType/response-vehicleType.mapper";
 import { VehicleTypeRepository } from "src/core/infrastructure/Repositories/vehicleType/vehicleType.repository";
 import { messages } from "src/core/infrastructure/Shared/messages";
 import { Result, result } from "src/core/infrastructure/Shared/result.util";
--- api/src/core/infrastructure/modules/parts.module.ts
+++ api/src/core/infrastructure/modules/parts.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../database/prisma.service';
 import { CqrsModule } from '@nestjs/cqrs';
 import { PartsController } from 'src/core/presentation/controllers/parts.controller';
-import { CommandPartHandlers, QueryPartHandlers } from 'src/core/application/handlers/PART.handlers';
 import { PartRepository } from '../Repositories/part/part.repository';
 import { PartPrismaRepository } from '../Repositories/part/part.prisma.repository';
+import { CommandPartHandlers, QueryPartHandlers } from 'src/core/application/handlers/part.handlers';
 
 
 @Module({
